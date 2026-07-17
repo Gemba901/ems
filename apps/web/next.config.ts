ccc5211e2023ececdd38bbfad71ef4f98685fda3
paulet-art <wanjirupaulet@gmai.com>
2026-07-17 13:29:21 +0300

    P01-A01 init commit

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: process.env.DOCKER_BUILD === "true" ? "standalone" : undefined,
+  allowedDevOrigins: ["192.168.100.19", "10.36.90.18", "localhost"],
 };
 
 export default nextConfig;
